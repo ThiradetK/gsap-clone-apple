b0ff3429bb65a937a0339c17b1dcded2d0a849b7
ThiradetK <thiradet.k28@gmail.com>
2025-10-22 00:29:30 +0700

    add SRGBcolor

--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -8,11 +8,10 @@ Source: https://sketchfab.com/3d-models/macbook-pro-m3-16-inch-2024-8e34fc2b3031
 Title: macbook pro M3 16 inch 2024
 */
 
-import React from 'react';
 import { useEffect } from 'react';
 import { useGLTF, useTexture } from '@react-three/drei';
 import useMacbookStore from '../../store';
-import { Color } from 'three';
+import { Color, SRGBColorSpace } from 'three';
 import { noChangeParts } from '../../constants';
 
 export default function MacbookModel16(props) {
@@ -22,6 +21,8 @@ export default function MacbookModel16(props) {
     '/models/macbook-16-transformed.glb'
   );
   const texture = useTexture('/screen.png');
+  texture.colorSpace = SRGBColorSpace;
+  texture.needsUpdate = true;
 
   useEffect(() => {
     scene.traverse((child) => {

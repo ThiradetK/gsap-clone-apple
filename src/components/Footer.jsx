277809896b4b4d8cee1769dba129cfbc6d1b79f3
ThiradetK <thiradet.k28@gmail.com>
2025-10-22 18:04:48 +0700

    add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,28 @@
+import { footerLinks } from '../constants/index';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you, Or
+          call
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>Copyright &copy; 2025 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
